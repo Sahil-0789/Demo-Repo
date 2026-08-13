b143581737b2679fc1177a45efd089de02bf89cc
Sahil-0789 <saierande7@gmail.com>
2026-08-13 14:31:18 +0530

    Adding project

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+app.get('/asd', (req, res) => {
+  res.send('Hello World from asd endpoint')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
